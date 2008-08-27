ae4d625d81837d223f49e9302ea0390e4d61c011
paul <paul@socialmoth.com>
2008-08-27 05:05:05 -0700

    made more raily

--- table_moth.rb
+++ table_moth.rb
@@ -1,39 +1,44 @@
 module TableMoth
 
-  def tablurize(collection, options = {:row_length => 4}, &block)
-    row_length = options[:row_length]
-    class_name = collection.first.class.to_s.downcase
+  def tablurize(collection, options = {}, &block)
+    row_length = options[:row_length] || 4
+    css_class = options[:css_class] || collection.first.class.to_s.downcase
     options[:first_row] = true
-    content_tag = "<table class=\"#{class_name.pluralize} gridlayout\">"
-    (1+collection.size/row_length).times do |row|
-      row_collection = collection[row*row_length,row_length]
-      content_tag += capture(row_collection, options, &block)
-      options[:first_row] = false
+    row_number = (1+collection.size/row_length)
+
+    content_tag :table, {:class => "#{css_class.pluralize} gridlayout"} do
+      rows = []
+      row_number.times do |row|
+        row_collection = collection[row*row_length,row_length]
+        rows << yield(row_collection, options)
+        options[:first_row] = false
+      end
+      rows.join
     end
-    content_tag += "</table>"
-    block_is_within_action_view?(block) ? concat(content_tag, block.binding) : content_tag
   end
 
-  def rowize(row_collection, options = {:row_length => 4}, &block)
-    content_tag = "<tr>"
-    class_name = row_collection.first.class.to_s.downcase
-
+  def rowize(row_collection, options, &block)
+    css_class = options[:css_class]
     width = (100.0/options[:row_length]).to_i
+    html_options = {:width => "#{width}%", :class => css_class, :valign => "middle"}
+    html_options[:style] = options[:style] if options[:style]
 
-    row_collection.each do |item|
-      content = capture(item, &block)
-      content_tag += "<td width=\"#{width}%\" class=\"#{class_name}\" #{"style=\"#{options[:style]}\"" if options[:style]} valign=\"middle\">#{content}</td>"
-    end
-
-    # attempt at filling out the row, so the item isn't centered
-    if options[:first_row]
-      (options[:row_length] - row_collection.size).times do |row|
-        content_tag += "<td width=\"#{width}%\" class=\"#{class_name}\" #{"style=\"#{options[:style]}\"" if options[:style]} valign=\"middle\"></td>"
+    content_tag :tr do
+      full = row_collection.collect do |item|
+        content_tag :td, html_options do
+          yield item
+        end
       end
-    end
-    content_tag += "</tr>"
 
-    block_is_within_action_view?(block) ? concat(content_tag, block.binding) : content_tag
+      # if first row is partially empty
+      if options[:first_row]
+        (options[:row_length] - row_collection.size).times do |row|
+          full << (content_tag :td, nil, html_options)
+        end
+      end
+
+      full.join
+    end
   end
 
 end

d535e2a6d8c6beb2633dbe866f8fee99d35d2cf6
Paul <paul.mckellar@gmail.com>
2009-02-02 22:48:59 -0800

    update tablemoth to rails 2.2.2

--- lib/table_moth.rb
+++ lib/table_moth.rb
@@ -17,7 +17,7 @@ module TableMoth
       end
       rows.join
     end
-    block_is_within_action_view?(block) ? concat(content, block.binding) : content
+    block_called_from_erb?(block) ? concat(content) : content
   end
 
   def rowize(row_collection, options, &block)
@@ -42,7 +42,67 @@ module TableMoth
 
       full.join
     end
-    block_is_within_action_view?(block) ? concat(content, block.binding) : content
+    block_called_from_erb?(block) ? concat(content) : content
   end
+
+
+  # string based concatination alternative
+
+  # def tablurize(collection, options = {}, &block)
+  #   return nil if collection.empty?
+  #
+  #   row_length = (options[:row_length] ||= 4)
+  #   css_class = (options[:css_class] ||= collection.first.class.to_s.downcase)
+  #   options[:first_row] = true
+  #   row_number = (1+collection.size/row_length)
+  #
+  #   content = "<table class=\"#{css_class.pluralize} gridlayout\">"
+  #
+  #   rows = []
+  #   row_number.times do |row|
+  #     row_collection = collection[row*row_length,row_length]
+  #     rows << capture(row_collection, options, &block)
+  #     options[:first_row] = false
+  #   end
+  #
+  #   content << "#{rows.join}</table>"
+  #
+  #   block_called_from_erb?(block) ? concat(content) : content
+  # end
+  #
+  # def rowize(row_collection, options, &block)
+  #   width = (100.0/options[:row_length]).to_i
+  #
+  #   td_options = <<-TDOPTIONS
+  #     width=\"#{width}%\" class=\"#{options[:css_class]}\" valign=\"middle\"
+  #   TDOPTIONS
+  #
+  #   td_options += "style=\"#{options[:style]}\"" if options[:style]
+  #
+  #   content = "<tr>"
+  #
+  #   tds = []
+  #
+  #   row_collection.each do |item|
+  #     tds << <<-TD
+  #       <td #{td_options}>
+  #         #{capture(item, &block)}
+  #       </td>
+  #     TD
+  #   end
+  #
+  #   # if first row is partially empty
+  #   if options[:first_row]
+  #     (options[:row_length] - row_collection.size).times do |row|
+  #       tds << <<-TD
+  #         <td #{td_options}></td>
+  #       TD
+  #     end
+  #   end
+  #
+  #   content << "#{tds.join}</tr>"
+  #
+  #   block_called_from_erb?(block) ? concat(content) : content
+  # end
 
 end
